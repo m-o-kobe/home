97d5e2ac42732cbd4ea0e53562d2fd4022454ef2
m-o-kobe <peachchild.be@gmail.com>
2019-08-26 13:20:36 +0900

    原さん用K関数完成

--- harasan/tenkatei.r
+++ harasan/tenkatei.r
@@ -2,13 +2,19 @@ library(spatstat)
 
 data<-read.csv("../../0822toutsuba.csv")
 pppdata<-ppp(x=data$X,y=data$Y,window=owin(c(-20,80),c(0,40)),marks=data$spp)
-kdata<-Kmulti(pppdata,marks(pppdata)=="tou",marks(pppdata)=="tsuba",correction="Ripley")
-ldata<-Lcross(pppdata,"tou","tsuba",correction="Ripley")
 
-k100<-envelope(pppdata,Kcross,i="tou",j="tsuba",correction="Ripley",nsim=100)
+k100<-envelope(pppdata,Kcross,i="tou",j="tsuba",correction="Ripley",nsim=1000,nrank=25)
+valuek<-fvnames(k100,a=".")
 
-k100as<-as.function(k100)
-print(k100as(1))
+
+k100as<-as.function(k100,value=valuek)
+
+kmatome<-data.frame(r=seq(0,10,0.01))
+kmatome$theo<-k100as(kmatome$r,"theo")
+kmatome$obs<-k100as(kmatome$r,"obs")
+kmatome$hi<-k100as(kmatome$r,"hi")
+kmatome$lo<-k100as(kmatome$r,"lo")
 png("../../harasan.png",height=960, width=960, res=144)
 plot(k100,sqrt(./pi)-r~r)
 dev.off()
+write.csv(kmatome,"../../harasan.csv")
