cc619b1b086ac2e0429abccb95a7f1cb86cc0ddf
m-o-kobe <peachchild.be@gmail.com>
2019-09-05 15:43:14 +0900

    jissendata

--- lecture/20190904~06/mari.R
+++ lecture/20190904~06/mari.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 DF <- read.csv("../../../marinair-pref.csv", header = TRUE, stringsAsFactors = FALSE)
 
-DFkansai<-subset(DF,(DF$都道府県コード<=30)&DF$都道府県コード>=24)
+DFkansai<-subset(DF,(DF$都道府県コード<=30)&(DF$都道府県コード>=24))
 g<-ggplot(data=DFkansai,aes(x=経過時間,y=人流指数,color=都道府県))+
 #geom_line()
 geom_bar(stat="identity",position="fill")
