703bfe981608d2893e64979e669baee9459277a3
m-o-kobe <peachchild.be@gmail.com>
2019-09-06 15:41:17 +0900

    jissendata

--- lecture/20190904~06/bunrui.R
+++ lecture/20190904~06/bunrui.R
@@ -2,6 +2,20 @@ library(doBy)
 library(dplyr)
 library(ggplot2)
 library(GGally)
+out<-"../../../kaiki.csv"
+tablecsv<-function(datalm,data,ou){
+    sum<-summary(datalm)
+    coe <- sum$coefficient
+    N <- nrow(data)
+    aic <- AIC(datalm)
+    result <- cbind(coe,aic,N)
+    result[2:nrow(result),5:6] <- ""
+    
+    write.table(matrix(c("\n",colnames(result)),nrow=1),ou,append=T,quote=F,sep=","
+    ,row.names=F,col.names=F)
+    write.table(result,ou,append=T,quote=F,sep=",",row.names=T,col.names=F)
+}
+
 DF <- read.csv("../../../marinair-pref.csv", header = TRUE, stringsAsFactors = FALSE)
 pop<- read.csv("../../../pop_dis.csv", header = TRUE, stringsAsFactors = FALSE)
 pop$kinki<-0
@@ -15,37 +29,40 @@ pop[pop$Prefecturecode==13,]$flight<-849615
 pop[pop$Prefecturecode==42,]$flight<-219194
 pop[pop$Prefecturecode==46,]$flight<-151524
 pop[pop$Prefecturecode==47,]$flight<-427342
-jinryu<-summaryBy(jinryu~Prefecturecode,data=DF,FUN=(mean))
-names(jinryu)[2]<-"jinryu"
+jinryu<-summaryBy(jinryu~Prefecturecode,data=DF,FUN=c(mean,length))
+jinryu$jinryu<-jinryu$jinryu.mean*jinryu$jinryu.length
+
 matome<-inner_join(pop,jinryu)
 
 
 lmresult<-lm(jinryu~Population+kinki+flight+distance,data=matome)
-summary(lmresult)
+tablecsv(lmresult,matome,out)
 slm<-step(lmresult)
-summary(slm)
+tablecsv(slm,matome,out)
+
 matome$distance_1<-1/matome$distance
 matome$distance_2<-1/(matome$distance)^2
 
 
 lmresult<-lm(jinryu~Population+flight+distance_1+Population*flight+distance_1*flight+Population*distance_1,data=matome)
-summary(lmresult)
+tablecsv(lmresult,matome,out)
 slm<-step(lmresult)
-summary(slm)
 
+tablecsv(slm,matome,out)
 lmresult<-lm(jinryu~Population+flight+distance_2+Population*flight+distance_2*flight+Population*distance_2,data=matome)
-summary(lmresult)
+tablecsv(lmresult,matome,out)
 slm<-step(lmresult)
 summary(slm)
+tablecsv(slm,matome,out)
 
-g<-ggpairs(matome[,-c(1,4,7,8)],aes(alpha=0.5))
+#g<-ggpairs(matome[,-c(1,4,7,8)],aes(alpha=0.5))
 #g<-ggplot(data=DF,aes(x=経過時間,y=人流指数,color=分類,group=都道府県))+
 #scale_y_continuous(limits = c(0, 2000))+
 #geom_line()
 #geom_bar(stat="identity",position="fill")
-png("../../../soukan.png",height=960, width=960, res=144)
-print(g)
-dev.off()
+#png("../../../soukan.png",height=960, width=960, res=144)
+#print(g)
+#dev.off()
 png("../../../kaikisindan.png",height=960, width=960, res=144)
 par(mfrow=c(2,2))
 plot(slm)
---
+++ lecture/20190904~06/crosscorrelation.R
@@ -0,0 +1,17 @@
+prex<-26
+prey<-27
+DF <- read.csv("../../../marinair-pref.csv", header = TRUE, stringsAsFactors = FALSE)
+xdata<-subset(DF,DF$Prefecturecode==prex)
+ydata<-subset(DF,DF$Prefecturecode==prey)
+x.ts<-as.ts(xdata$jinryu)
+y.ts<-as.ts(ydata$jinryu)
+print(x.ts)
+print(y.ts)
+#g<-ggplot(data=DF,aes(x=経過時間,y=人流指数,color=分類,group=都道府県))+
+#scale_y_continuous(limits = c(0, 2000))+
+#geom_line()
+#geom_bar(stat="identity",position="fill")
+png("../../../26_27.png",height=960, width=960, res=144)
+ccf(x.ts,y.ts)
+
+dev.off()
